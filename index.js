c03cfa96499f1d518d3f3eca02bb778b23b71085
Peter Ullrich <peda76@googlemail.com>
2018-01-10 17:13:36 +0100

    -Missing DisplayBuilder export -get [Symbol.toStringTag]()

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.CardBuilder = require('./lib/CardBuilder');
+module.exports.DisplayBuilder = require('./lib/DisplayBuilder');
 module.exports.ResponseBuilder = require('./lib/ResponseBuilder');
 module.exports.SsmlBuilder = require('./lib/SsmlBuilder');
