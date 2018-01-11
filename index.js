405f06b2b3452f4d7785ef2bdcaad1900b3f8c7d
Peter Ullrich <peda76@googlemail.com>
2018-01-11 20:53:30 +0100

    TextBuilder.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ module.exports.CardBuilder = require('./lib/CardBuilder');
 module.exports.DisplayBuilder = require('./lib/DisplayBuilder');
 module.exports.ResponseBuilder = require('./lib/ResponseBuilder');
 module.exports.SsmlBuilder = require('./lib/SsmlBuilder');
+module.exports.TextBuilder = require('./lib/TextBuilder');
